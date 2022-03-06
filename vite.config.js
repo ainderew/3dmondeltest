d496ece762ab64f514936f09065d764da76cf764
Andrew Pinon <andrewapinon@gmail.com>
2022-03-06 15:05:36 +0800

    changes to directory for the gltf model

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,22 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.gltf']
-  
-})
-
-module.exports = {
+  assetsInclude: ['**/*.gltf'],
   root: "./",
   build:{
     ourDir: "dist"
   },
   publicDir: "assets"
-}
+  
+})
+
+// module.exports = {
+//   root: "./",
+//   build:{
+//     ourDir: "dist"
+//   },
+//   publicDir: "assets"
+// }
 
 
 
