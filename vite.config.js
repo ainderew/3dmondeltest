a5b036ac5095165890eae25b316d144d9fdea95a
Andrew Pinon <andrewapinon@gmail.com>
2022-03-06 17:51:21 +0800

    choco

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.gltf', '**/*.png','**/*.txt', '**/*.bin'],
+  assetsInclude: ['**/*.gltf', '**/*.png','**/*.txt', '**/*.bin','**/*.glb'],
   root: "./",
   build:{
     outDir: "dist"
